e27f72af1c3b1af32c63f66b5dfcbe462786d3f6
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-08 21:49:44 +0530

    day12 ass sub

---
+++ Day12/Day12 project1/Day12 project1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_project1
+{
+    /********************************************************************
+     * Author:Bhanu rama krishna prakash jakkmsetti
+     * purpose:exception handling
+     * ***********************************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                int a, b, c;
+                Console.WriteLine("enter a");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter b");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine(c);
+                int[] arr = new int[5];
+                arr[8] = 1;
+                Console.ReadLine();
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("enter value in 0-5000 only");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("divide by zero will not allow");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("in correct format or input");
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("check the index value is with in range only");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("contact bhanu ");
+            }
+        }
+    }
+}
+
---
+++ Day12/Day12 project4/Day12 project4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_project4
+{
+    internal class Program
+    {
+
+       static void Print(int a)
+        {
+            Console.WriteLine(a);
+            Print(++a);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Print(0);
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine("recursion error");
+                Console.ReadLine();
+            }
+        }
+    }
+}
