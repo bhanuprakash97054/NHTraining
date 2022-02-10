997d56b7bfa749b0f1ab4265a469bd88fd5d4ca6
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 20:22:41 +0530

    using continue keyword

---
+++ Day14/Day14 project5/Day14 project5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project5
+{
+    /***********************************************************
+     * Author:bhanu rama krishna prakash jakkamsetti
+     * purpose:use continue keyword
+     * **********************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=30; i++)
+            {
+                if (i%3==0)
+                 continue;
+                    Console.Write($"{i} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
