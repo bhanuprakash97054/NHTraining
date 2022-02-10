5b6f2df4d4f648113bf582469e20ec59540c3a24
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 20:13:04 +0530

    is prime or not by using break

---
+++ Day14/Day14 project4/Day14 project4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project4
+{
+    /*********************************************************
+     * Author:bhanu rama krishna prakash jakkamsetti
+     * purpose:print prime by using break
+     * *********************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 78, i;
+            for ( i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    break;
+            }
+            if (i == n)
+            {
+                Console.WriteLine("is prime");
+            }
+            else 
+            { 
+                Console.WriteLine("not prime");
+            }
+        }
+    }
+}
