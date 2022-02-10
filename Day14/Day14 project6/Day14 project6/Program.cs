1ae5e79fe70efee18b826b426c603ec1012a8a3f
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 20:33:01 +0530

    first number after 1000 which is divisible by 97 using break

---
+++ Day14/Day14 project6/Day14 project6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project6
+{
+    /*************************************************************************
+     * Author:bhanu rama krishna prakash jakkamsetti
+     * purpose:first number after 1000 which is divisible by 97
+     * ***********************************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =1000; i <= 1097; i++)
+            {
+                if (i % 97 == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
