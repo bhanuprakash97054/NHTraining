946cc47a0762bfb3e4b7aac297f21bd5ff328ca5
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 19:07:16 +0530

    creating sealed class

---
+++ Day14/Day14 project1/Day14 project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project1
+{
+   sealed class Police
+    {
+        public static int EmgNumber = 100;
+        public string TakeAction()
+        {
+            return "Tke Action immidiatly";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Police p = new Police();
+            Console.WriteLine(Police.EmgNumber);
+            Console.WriteLine(p.TakeAction());
+        }
+    }
+}
