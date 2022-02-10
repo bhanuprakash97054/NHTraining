c14efbd94689c41df2fc3c011f5f06408bb5b4b1
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 21:23:02 +0530

    day14 ass sub

---
+++ Day14/Day14 project7/Day14 project7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project7
+{
+    /*************************************************************************
+    * Author:bhanu rama krishna prakash jakkamsetti
+    * purpose:declaring normal method in interface
+    * ***********************************************************************/
+    interface IStudent
+    {
+        int Id { get; set; }   
+        string Name { get; set; }
+
+        public void PrintHi();
+       
+    }
+    class Lol : IStudent
+    {
+        public void PrintHi()
+        {
+            Console.WriteLine("hiiiiii");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
