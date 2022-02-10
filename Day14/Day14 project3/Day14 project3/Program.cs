52930bd9f4dabd1aa517b20a66de931a59307708
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-10 19:59:20 +0530

    initialize auto implemented properties

---
+++ Day14/Day14 project3/Day14 project3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_project3
+{/***********************************************************************
+     * Author:bhanu rama krishna prakash jakkamsetti
+     * Purpose:initializing auto implemented properties
+     * ***************************************************************************/
+
+    class Averagespeed
+    {
+        private int time;
+        private int distance;
+       
+        public int Time
+        {
+            set { time = value; }
+        }
+        public int Distance
+        {
+            set { distance = value; }
+        }
+        public int Speed
+        {
+            get
+            {
+               return distance / time;
+                
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Averagespeed a = new Averagespeed();
+                a.Time = 30;
+                a.Distance = 120;
+                Console.WriteLine(a.Speed);
+            }
+        }
+    }
+}
