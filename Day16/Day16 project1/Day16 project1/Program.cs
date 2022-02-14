2c65c3630bfe095264ccc5a3b2f0fb4b8ff3a2a0
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-14 10:33:40 +0530

    print hello world by using object oriented way

---
+++ Day16/Day16 project1/Day16 project1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_project1
+{
+    class HelloWorld
+    {
+        public void ReadData()
+        {
+            Console.WriteLine("enter data");
+            string s=Console.ReadLine();    
+        }
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld h = new HelloWorld();
+            h.ReadData();
+           
+            Console.ReadLine();
+        }
+    }
+}
