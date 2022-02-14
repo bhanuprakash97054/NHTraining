72be64da8e6760546279dc527d560de8b15bb902
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-14 11:36:42 +0530

    creating library

---
+++ Day16/BhanuLibrary/BhanuLibrary/Mathematics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhanuLibrary
+{
+    /***************************************************
+ * Author:bhanu rama krishna prakash jakkamsetti
+ * purpose:creating library
+ * ************************************************/
+
+    internal class Mathematics
+    {
+        int fact = 1, n;
+        /// <summary>
+        /// read data for factorial
+        /// </summary>
+        public void ReadData()
+        {
+            Console.WriteLine("enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// find factorial
+        /// </summary>
+        /// <returns>factorial</returns>
+        public int Factorial()
+        {
+            for (int i = 1; i <= n; i++)
+                fact = fact * i;
+            return fact;
+        }
+
+
+    }
+}
