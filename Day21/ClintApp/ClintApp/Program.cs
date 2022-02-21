3cb3cf3caa27724b33c5b40d428bbc8e309fc5bf
bhanuprakash97054 <bhanuprakash97054@gmail.com>
2022-02-21 11:54:57 +0530

    day21 assignment sub

---
+++ Day21/ClintApp/ClintApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClintApp.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClintApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient w = new WebService1SoapClient();
+            Console.WriteLine(w.Factorial(5));
+            Console.WriteLine(w.Add(15,5));
+            Console.WriteLine(w.Mul(15,5));
+            Console.WriteLine(w.Div(15,5));
+        }
+    }
+}
---
+++ Day21/WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForms.ServiceReference1;
+
+namespace WindowsForms
+{
+    /**********************************
+     * author:bhanu rama krishna prakash jakkamsetti
+     * purpose:access web server by using windows app
+     * ***************************************/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           int n=Convert.ToInt32(textBox1.Text);
+            WebService1SoapClient w = new WebService1SoapClient();
+            textBox2.Text = w.Factorial(n).ToString();        
+        }
+    }
+}
